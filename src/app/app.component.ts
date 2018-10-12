28ff53f4579d7a474c1ca10242aa83a62c7bd242
Çağdaş IŞIK <cisik@innova.com.tr>
2018-10-12 13:26:59 +0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,103 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { TodoService } from './services/todo.service';
+import { TodoModel } from './models/todo.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+  error: Object;
+  todos: TodoModel[] = [];
+  loading: boolean = false;
+  newTodo: TodoModel = {
+    id: null,
+    title: '',
+    completed: false
+  };
+
+  newTodoForm: FormGroup = this.fb.group({
+    title: ''
+  })
+
+  constructor(private todoService: TodoService, private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.getTodos();
+  }
+
+  reloadList() {
+    this.getTodos();
+  }
+
+  handleSubmit() {
+    this.newTodo.title = this.newTodoForm.value.title;
+    this.addTodo(this.newTodo);
+  }
+
+  getTodos() {
+    this.loading = true;
+    this.todoService.getTodos().subscribe((todos: TodoModel[]) => {
+      this.loading = false;
+      this.error = null;
+      this.todos = todos;
+    }, (error) => {
+      this.loading = false;
+      this.error = error;
+    });
+  }
+
+  addTodo(addedTodo) {
+    this.loading = true;
+    return this.todoService.addTodo(addedTodo).subscribe((todo: TodoModel) => {
+      this.loading = false;
+      this.error = null;
+      this.todos = [ ...this.todos, todo ];
+    }, (error) => {
+      this.loading = false;
+      this.error = error;
+    }, () => {
+      this.newTodoForm.patchValue({
+        title: ''
+      })
+    });
+  }
+
+  updateTodo(updatedTodo): void {
+    this.loading = true;
+    updatedTodo.completed = !updatedTodo.completed;
+    this.todoService.updateTodo(updatedTodo).subscribe(
+      (result: TodoModel) => {
+        this.loading = false;
+        this.error = null;
+        this.todos = this.todos.map((todo) => {
+          if(result.id === todo.id) {
+            todo = updatedTodo;
+          }
+
+          return todo;
+        })
+      }, (error) => {
+        this.loading = false;
+        this.error = error;
+      }
+    );
+  }
+
+  deleteTodo(deletedTodo): void {
+    this.loading = true;
+    this.todoService.deleteTodo(deletedTodo).subscribe(
+      () => {
+        this.loading = false;
+        this.error = null;
+        this.todos = this.todos.filter((todo) => todo.id !== deletedTodo.id);
+      }, (error) => {
+        this.loading = false;
+        this.error = error;
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule }   from '@angular/forms';
+import { TodoService } from './services/todo.service';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
