bf6e43681e399b8325d5335bf698c8e3b20d79df
cyasam <cyasam@gmail.com>
2018-10-15 03:11:50 +0300

    Edit Form added.

--- src/app/components/todo/todo-list/todo-list.component.ts
+++ src/app/components/todo/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { TodoModel } from '../../../models/todo.interface';
 
 @Component({
@@ -6,7 +7,7 @@ import { TodoModel } from '../../../models/todo.interface';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
 
   @Input() todos: TodoModel[];
   @Input() loading: boolean;
@@ -15,8 +16,32 @@ export class TodoListComponent {
   @Output() updateTodo: EventEmitter<TodoModel> = new EventEmitter();
   @Output() deleteTodo: EventEmitter<TodoModel> = new EventEmitter();
 
+  todoForm: FormGroup = this.fb.group({
+    title: this.todos
+  });
+  openEditForm: boolean = false;
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    console.log(this.todoForm)
+    this.todoForm.statusChanges.subscribe(value => {
+      console.log(value);
+    })
+  }
+
   handleUpdateTodo(todo) {
     this.updateTodo.emit(todo);
+  }
+
+  handleEditTodo(todo){
+    todo.title = this.todoForm.value.title;
+    this.handleUpdateTodo(todo);
+    this.toggleEditTodo();
+  }
+
+  toggleEditTodo(){
+    this.openEditForm = !this.openEditForm;
   }
   
   handleDeleteTodo(todo) {
